d4f7ad2dfa58824141450770cc75eb5081accdff
Akhu <anthony.dacr@gmail.com>
2015-06-15 18:06:48 +0200

    First Commit

---
+++ Cinmple/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cinmple
+//
+//  Created by Anthony Da cruz on 15/06/2015.
+//  Copyright (c) 2015 Fallingstarz. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
